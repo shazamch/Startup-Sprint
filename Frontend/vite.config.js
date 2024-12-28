fd96fed37d0bee5ba8ec9c044134345e0f0d5976
shazamch <shazamrazzaq1234@gmail.com>
2024-12-28 14:59:32 +0500

    Add build output directory configuration to Vite setup

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,16 +7,19 @@ export default defineConfig({
   plugins: [
     react(),
     compression({
-      verbose: true,       // Log compression details to the console
-      disable: false,      // Enable compression
-      threshold: 10240,    // Compress files larger than 10 KB
-      algorithm: 'gzip',   // Use gzip compression (you can also use 'brotli' here)
-      ext: '.gz',          // Set the extension for compressed files
+      verbose: true, 
+      disable: false,
+      threshold: 10240,
+      algorithm: 'gzip',
+      ext: '.gz',
     }),
   ],
   resolve: {
     alias: {
       "./runtimeConfig": "./runtimeConfig.browser",
     },
-  }
+  },
+  build: {
+    outDir: 'build',
+  },
 })
