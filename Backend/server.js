362b68984efd0b9230af46437d5c2018e5e51cdd
shazamch <shazamrazzaq1234@gmail.com>
2024-12-28 17:38:35 +0500

    Updated CORS configuration to remove port from allowed origin

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:5173',"http://52.71.51.45:8080"],
+        origin: ['http://localhost:5173',"http://52.71.51.45"],
         methods: ['GET', 'POST'],
         credentials: true,
     },
